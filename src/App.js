c1198578445712519a0bf91614dc5ca74fabf5d6
jarintasnim00 <jarin35-1641@diu.edu.bd>
2025-08-16 21:54:59 +0600

    Added route in this App

--- src/App.js
+++ src/App.js
@@ -3,19 +3,59 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import About from './Components/About';
+import Alert from './Components/Alert';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 
 
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not.
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type    
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = ()=> {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      document.title = "TextUtils - Dark Mode";
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.title = "TextUtils - Light Mode";
+      showAlert("Light mode has been enabled", "success");
+    }
+  } 
+  
+  
   return (
     <>
-   
-<Navbar title = "React" home="HomePage" />
-<div className="container my-3">
-  <TextForm heading="Enter the text to analysis"/> 
-  <About/>
-</div>
-
+     <Router>
+        <Navbar title="React" home="HomePage" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analysis"/>} />
+          </Routes>
+        </div>
+    </Router>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -6,17 +6,20 @@ export default function TextForm(props) {
     const handleUpClick = () => {
     //   console.log("Uppercase was clicked" + text);
      let newText= text.toUpperCase();
-        setText(newText);
+        setText(newText)
+        props.showAlert("Converted to uppercase", "success");
     }
 
     const handleLoClick = () => {
     //   console.log("Uppercase was clicked" + text);
      let newText= text.toLowerCase();
-        setText(newText);
+        setText(newText)
+        props.showAlert("Converted to lowercase", "success");
     }
     const handleClearClick = () => {
          let newText= '';
         setText(newText);
+        props.showAlert("Text cleared", "success");
     }    
     const handleOnChange = (event) => {
       //console.log("On change");
@@ -26,20 +29,20 @@ export default function TextForm(props) {
   return (
     <div>
           <>
-          <div className="container">
+          <div className="container"  style={{color:props.mode === 'dark'?'white':'#042743' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode === 'dark'?'grey':'white', color:props.mode === 'dark'?'white':'#042743'  }} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To toLowerCase</button>
                 <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
           </div>
-          <div className="container my-3">
+          <div className="container my-3" style={{color:props.mode === 'dark'?'white':'#042743' }}>
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} character in this box</p>
             <h3>Preview</h3>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter text to preview here...." }</p>
           </div>
           
           </>
